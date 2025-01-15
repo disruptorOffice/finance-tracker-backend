314c9d57639533788320c3f6205442780b9208f6
Alberto Avantes <alberto.avantes@gmail.com>
2025-01-15 00:06:44 -0600

    improvement: cambiamos extension de archivos a js

---
+++ src/routes/user.route.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { UserService } from "../services/user.service";
+
+const userService = new UserService();
+
+const router = express.Router();
+router.use(express.json());
+
+router.post("/", async (req, res, next) => {
+  try {
+    const result = await userService.storeUser(req.body);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:user_id/finances/:finance_id", async (req, res, next) => {
+  const { user_id, finance_id } = req.params;
+  const payload = req.auth; // `req.auth` contiene los datos del token decodificado
+
+  try {
+    const result = await userService.retrieveOneFinance(
+      user_id,
+      finance_id,
+      payload
+    );
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:user_id/finances", async (req, res, next) => {
+  const { user_id } = req.params;
+  const payload = req.auth; // `req.auth` contiene los datos del token decodificado
+
+  try {
+    const result = await userService.retrieveFinances(user_id, payload);
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
